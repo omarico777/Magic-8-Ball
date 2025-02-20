be7122749f6da43f5e3509f82d1d94b5c5322201
omarico <138431257+omarico777@users.noreply.github.com>
2025-02-20 13:40:29 +0200

    wegfweg

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 def res():
     print("\n the magic 8 ball says",response,"\n")
+
+def play_again():
+    while True:
+        choice = input("do you want to ask a question(y/n):").strip().lower()
+        if choice == "y":
+            return True
+        elif choice == "no":
+            return False
+        else:
+            print("plz type y/n")
